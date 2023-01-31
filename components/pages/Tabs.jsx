97b668d579062b6615608900f4e3c6b3b7b1f05e
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-31 17:26:15 +0800

    update working,

--- components/pages/Tabs.jsx
+++ components/pages/Tabs.jsx
@@ -4,6 +4,7 @@ import { IonReactRouter } from '@ionic/react-router';
 import { cog, flash, list } from 'ionicons/icons';
 
 import Home from './Feed';
+import Helloworld from './Helloworld';
 import Lists from './Lists';
 import ListDetail from './ListDetail';
 import Settings from './Settings';
@@ -12,22 +13,30 @@ const Tabs = () => {
   return (
     <IonTabs>
       <IonRouterOutlet>
+        <Route path="/tabs/helloworld" render={() => <Helloworld />} exact={true} />
         <Route path="/tabs/feed" render={() => <Home />} exact={true} />
         <Route path="/tabs/lists" render={() => <Lists />} exact={true} />
         <Route path="/tabs/lists/:listId" render={() => <ListDetail />} exact={true} />
         <Route path="/tabs/settings" render={() => <Settings />} exact={true} />
-        <Route path="/tabs" render={() => <Redirect to="/tabs/feed" />} exact={true} />
+        <Route path="/tabs" render={() => <Redirect to="/tabs/helloworld" />} exact={true} />
       </IonRouterOutlet>
       
       <IonTabBar slot="bottom">
+        <IonTabButton tab="tab_helloworld" href="/tabs/helloworld">
+          <IonIcon icon={flash} />
+          <IonLabel>Helloworld</IonLabel>
+        </IonTabButton>
+
         <IonTabButton tab="tab1" href="/tabs/feed">
           <IonIcon icon={flash} />
           <IonLabel>Feed</IonLabel>
         </IonTabButton>
+
         <IonTabButton tab="tab2" href="/tabs/lists">
           <IonIcon icon={list} />
           <IonLabel>Lists</IonLabel>
         </IonTabButton>
+
         <IonTabButton tab="tab3" href="/tabs/settings">
           <IonIcon icon={cog} />
           <IonLabel>Settings</IonLabel>
