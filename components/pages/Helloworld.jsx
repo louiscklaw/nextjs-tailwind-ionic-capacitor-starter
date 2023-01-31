8c697dea03f8deb6dcb811193d541043e02adf05
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-31 20:54:00 +0800

    update working,

--- components/pages/Helloworld.jsx
+++ components/pages/Helloworld.jsx
@@ -26,7 +26,7 @@ const Helloworld = () => {
           </IonToolbar>
         </IonHeader>
         <IonContent>
-          <div className="flex content-center items-center justify-center h-full">
+          <div className="flex content-center items-center justify-center h-full bg-blueGray-800">
             <div className="w-full lg:w-4/12 px-4">
               <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
                 <div className="rounded-t mb-0 px-6 py-6">
