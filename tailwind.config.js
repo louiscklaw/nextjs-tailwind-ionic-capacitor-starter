ac2ad4ad0b5efdffb71c5f5de365409f0297e9cb
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-31 20:06:58 +0800

    update working,

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
-
+const plugin = require("tailwindcss/plugin");
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   purge: [],
   darkMode: 'media',
   theme: {
+    colors: {
+      ...colors,
+    },
     extend: {},
   },
   variants: {
